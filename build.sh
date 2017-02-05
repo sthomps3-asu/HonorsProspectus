7c695bf016a9e66de5471b9675697864644bba78
Stephen Thompson <stephenbobthompson@gmail.com>
2017-02-05 02:48:44 -0700

    COMMIT NOTE: Initial commit. Establish directory structure. Added build script. Started topic overview.

---
+++ build.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+if which pdflatex
+then
+    DOCNAME=HonorsProspectus
+    TEXMAINFILE=main.tex
+    PASSES=2
+    OUTPUT_DIR=output
+    mkdir -p ${OUTPUT_DIR}
+    for ((i=0; i<${PASSES}; i++))
+    do
+        pdflatex -halt-on-error -file-line-error -interaction nonstopmode -jobname=${DOCNAME} -output-directory=${OUTPUT_DIR} ${TEXMAINFILE}
+        if [ $? ]
+        then
+            break
+        fi
+    done
+else
+    echo "pdflatex compiler not supported machine. Install pdflatex to build document"
+fi
